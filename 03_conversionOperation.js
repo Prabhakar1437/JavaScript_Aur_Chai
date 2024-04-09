5acd8f79c05f28f6047c8a86fa913af02feb04d1
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-09 12:39:15 +0530

    JavaScript

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -19,6 +19,39 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let SomeNumber = 33
-let stringNumber = String(SomeNumber)
-console.log( typeof stringNumber);
+// let SomeNumber = 33
+// let stringNumber = String(SomeNumber)
+// console.log( typeof stringNumber);
+
+//******************** Operation ******* 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+// let str1= "Prabhakar"
+// let str2= "  Vats"
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
---
+++ datatypes-summary.js
@@ -0,0 +1,26 @@
+//Primitive
+//& types: String,Number,Boolean,null,undefined,symbol,bigint
+
+
+//Reference (Non primitive)
+//Array,Objects,Functions
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id);
+// console.log(anotherId);
+
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myObj ={
+    name:"Vats",
+    age:22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+
+}
