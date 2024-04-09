845b7599da3c39341544e74acda573ddd9733588
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-09 10:59:52 +0530

    JavaScipt Aur Chai

---
+++ 01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 144424
+let accountEmail= "hitesh@googele.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPassword="213476"
+accountCity="Begusarai"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/* 
+Prefer not to use var because of issue in block scope and functional scope
+
+*/
---
+++ 02_Data_type.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello") we are using nodejs, not browser
+let name = "Vats"
+let age = 23
+
+//number
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>standalone value -> object
+//undefined => not defined -> undefined
+//symbol =>used to find unique
+
+//object
+
+console.log(typeof "Vats");
+
