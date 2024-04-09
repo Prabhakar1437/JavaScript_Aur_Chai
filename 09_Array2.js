8d53462b6f831e5aa2eba44dcfa265729d5353fa
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-09 18:22:31 +0530

    Array

---
+++ 09_Array2.js
@@ -0,0 +1,17 @@
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+//marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allheros =marvel_heros.concat(dc_heros)
+
+// console.log(allheros);
+
+// const all_new_heros = [...marvel_heros, ...dc_heros] //Spared operator
+
+// console.log(all_new_heros);
+
+
