259a443f46bd7e945560d228527473b16add27a0
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-09 11:16:10 +0530

    DataType

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = 33
+// console.log( typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score)
+// console.log( typeof valueInNumber);
+
+
+//"33" => 33
+//"33abc"=> NaN
+//true=> 1; false => 0
+
+//""=> false
+//"Vats"=> true
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let SomeNumber = 33
+let stringNumber = String(SomeNumber)
+console.log( typeof stringNumber);
